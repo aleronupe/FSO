e2a347bfa187105ff7b222a25d5050c715857dc7
Alexandre Miguel <aleronupe@gmail.com>
2018-11-27 11:48:49 -0200

    Added reading

--- Projeto_2/projeto2.c
+++ Projeto_2/projeto2.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
 
 //Struct que será utilizada para montar a TLB
 typedef struct tlb{
@@ -16,34 +15,31 @@ typedef struct address{
   unsigned int pure_numb;
   int page_numb;
   int disp_numb;
-} Adress;
+} Address;
 
 //Declaração da thread que lê os endereçoss
-void *load_addresses(void *param);
+void load_addresses(Address *logical, char *name_file);
 
 int main(int argc, char *argv[]){
 
   //ponteiro de endereços a ser criado
-  Adress *logical;
+  Address *logical;
 
   //Testa a thread "load_Addresses" que carrega os endereços pra um vetor de Adress
-  pthread_t load_id;
-  pthread_create(&load_id, NULL, load_addresses, logical);
-  pthread_join(load_id, NULL);
+  load_addresses(logical, argv[1]);
 
 
 
   return 0;
 }
 
-void *load_addresses(void *param) {
+void load_addresses(Address *logical, char *name_file) {
   //Lê a quantidade de endereços no arquivo a partir do número de espaços mais um
-  Adress *logical = *((address *) param)
   FILE *fp;
   char add;
   int add_numb = 0, cont;
 
-  fp = fopen("addresses.txt", "r");
+  fp = fopen(name_file, "r");
   while( fscanf(fp, "%c", &add) != EOF){
     if(add == 32){
       add_numb++;
@@ -54,18 +50,32 @@ void *load_addresses(void *param) {
   printf("O Programa contém %d enderecos\n", add_numb);
 
   //Modifica o endereço apontado pelo ponteiro na main com endereços
-  logical = (Address *) calloc(add_numb, sizeof(Address));
+  logical = (Address *) malloc(add_numb*sizeof(Address));
+
+  rewind(fp);
 
   unsigned int pure_address = 0;
   int page_address = 0, disp_address = 0;
 
   for(cont = 0; cont < add_numb; cont++){
-    
+      fscanf(fp, " %u", &pure_address);
+      page_address = ((0xFFFF & pure_address) >> 8);
+      disp_address = (0x00FF & pure_address);
 
 
+      (*(logical + cont)).pure_numb = pure_address;
+      (*(logical + cont)).page_numb = page_address;
+      (*(logical + cont)).disp_numb = disp_address;
+
+      printf("[%d]", cont);
+      printf("endereço lido: %u\n", (*(logical + cont)).pure_numb);
+      printf("pagina: %d\n", (*(logical + cont)).page_numb);
+      printf("deslocamentos: %d\n", (*(logical + cont)).disp_numb);
 
   }
 
 
-  pthread_exit(0);
+
+
+  return;
 }
